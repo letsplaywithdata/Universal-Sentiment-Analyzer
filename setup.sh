63a8a03cd13857627b0fe44b3aa7c6bac1170408
letsplaywithdata <50980928+letsplaywithdata@users.noreply.github.com>
2020-08-07 17:37:54 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
 mkdir -p ~/.streamlit/
+
 echo "\
 [general]\n\
 email = \"yogeshnizzer302@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
+
 echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
+" > ~/.streamlit/config.toml
